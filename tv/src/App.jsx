41b6ee044a169607c2244d1d30fd3fd45e019dfa
victor <victorpedr879@gmail.com>
2023-08-03 13:19:26 -0300

    feat: making navbar

--- tv/src/App.jsx
+++ tv/src/App.jsx
@@ -1,10 +1,11 @@
+import "./App.css";
 
-import './App.css';
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Ola Mundo</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ tv/src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Nav } from "./StyledNavbar";
+
+function Navbar() {
+  return (
+    <Nav>
+      <div>
+        <h1>StreamVision TV</h1>
+      </div>
+    </Nav>
+  );
+}
+
+export default Navbar;
